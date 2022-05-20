d9cb7844a91b18b0b94726f7debe60962a587ee9
Lucasfullstackker <cavalcantelucas20@hotmail.com>
2022-05-19 21:12:44 -0300

    secret word

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante", "Marcha"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Morango", "Abacaxi", "Melão", "Melancia", "Goiaba"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Cabeça", "Pés"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    hardware: ["Memória", "Processador", "SSD", "HD", "Cooler"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  };
